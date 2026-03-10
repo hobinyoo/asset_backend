c3b423f5c6b2278c0fb1ccca3fba7064b3c889c6
hobinyoo-acro <hb.yoo@acrofuture.com>
2026-03-10 16:22:30 +0900

    fix: add domain to CORS config

--- src/main/java/com/asset/asset_backend/common/config/CorsConfig.java
+++ src/main/java/com/asset/asset_backend/common/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000","https://asset-frontend-dusky.vercel.app/assets")
+                        .allowedOrigins("http://localhost:3000","https://yoojoo-asset-management.xyz")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
